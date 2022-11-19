e7a7bb8c2f2deff967f70edd60533ad105d85e79
Timur Galdin <galdin.timur508@gmail.com>
2022-11-19 18:48:35 +0300

    ts friendly changes

---
+++ src/views/Main.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+export default {};
+</script>
+
+<template lang="pug">
+.content
+  .header
+    .name
+    .progression
+</template>
+
+<style scoped lang="sass">
+.content
+  background-color: purple
+  width: 60vw
+  height: 90vh
+  margin: 4rem auto 0
+</style>
