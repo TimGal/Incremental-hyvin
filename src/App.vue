a5f758d35c09da191c710fcf1b51b3310e6392be
Timur Galdin <galdin.timur508@gmail.com>
2022-11-19 18:02:44 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+.content
+  .header
+    .name
+    .progression
+</template>
+
+<style scoped lang="sass">
+.content
+  background-color: purple
+  width: 60vw
+  height: 90vh
+  margin: 4rem auto 0
+</style>
